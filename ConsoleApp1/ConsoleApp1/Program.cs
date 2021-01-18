2ea7f99242f0dbaf05cf34a351163caae6ddab79
CUONG PHAM CAO <1821050741@student.humg.edu.vn>
2021-01-18 23:14:22 +0700

    LTQL

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello Word!");
+                Console.Write("Nhap ho va ten: ");
+
+            Console.WriteLine("Chao mung " + Console.ReadLine() + " den voi mon hoc LTQL");
+            }
+        }
+    }
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giaiptbac1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap he so a:");
+            float a = float.Parse(Console.ReadLine());
+            Console.Write("Nhap he so b:");
+            float b = float.Parse(Console.ReadLine());
+            if (a == 0)
+            {
+                
+            
+                    Console.WriteLine("Phuong trinh vo nghiem");
+            }
+            else
+            {
+                float x = -b / a;
+                Console.WriteLine("{0}x + {1} = 0 \n => x={2}", a, b, x);
+            }
+            Console.ReadKey();
+        }
+    }
+}
